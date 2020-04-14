a478b5353f58bbcbcba7e46c1df13e07a7c08dee
hanemario <amztmtm@gmail.com>
2020-04-14 15:58:06 +0900

    add privete router

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,8 +16,9 @@ firebase.initializeApp(firebaseConfig);
 // firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 
 // database.ref('expenses').on('child_removed', (snapshot) => {
