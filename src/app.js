7b3af023aa879d4a50e46101e012e15950baa3a0
hanemario <amztmtm@gmail.com>
2020-05-12 16:00:55 +0900

    add

--- src/app.js
+++ src/app.js
@@ -16,16 +16,11 @@ import LoadingPage from './components/LoadingPage'
 
 
 const store = configureStore();
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
-
 let hasRendered = false;
 const renderApp = () => {
   if (!hasRendered) {
@@ -39,11 +34,10 @@ ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     store.dispatch(login(user.uid));
-    console.log('uid', user.uid);;
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
-        history.push('/dashboard')
+        history.push('/dashboard');
       }
     });
   } else {
